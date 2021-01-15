3897ca167556f2365af2aaaa14f95b9fc8c1c621
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-15 11:27:28 -0800

    Create joke.model.js, Joke model with attributes validations

---
+++ server/models/jokes.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+
+
+const JokeSchema = new mongoose.Schema(
+    {
+        setup: {
+            type: String,
+            required: [true, "Setup required!"],
+            minlength: [10, "Setup must be at least 10 characters!"]
+        },
+        punchline: {
+            type: String,
+            required: [true, "Punchline required!"],
+            minlength: [3, "Punchline must be at least 10 characters!"]
+        }
+    },
+    { timestamps: true }
+);
+
+
+const Joke = mongoose.model("Joke", JokeSchema);
+
+module.exports = Joke;
